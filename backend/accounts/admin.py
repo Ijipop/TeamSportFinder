65bceae405327c06910919b4ef2961d7832a6909
Natacha <natachameyer@live.com>
2025-11-22 12:53:41 -0500

    Add tests

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User as DjangoUser
+
 from accounts.models import User
+
+# Supprimer les modèles par défaut de l’admin
+admin.site.unregister(Group)
+admin.site.unregister(DjangoUser)
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
