4ffaae69939e2c0b0fd035a355f92f5e90ff5388
dracken24 <dracken24@gmail.com>
2025-11-23 15:49:59 -0500

    merge complete

--- frontend/TeamSportFinder/src/pages/GestionEquipeOrganiser.tsx
+++ frontend/TeamSportFinder/src/pages/GestionEquipeOrganiser.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Container, Typography } from '@mui/material'
+import { Container, Typography, Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 const GestionEquipeOrganiser = () =>
 {
+	const navigate = useNavigate();
+	
 	return (
 		<Container maxWidth="lg" sx={{ mt: { xs: 8, sm: 10 }, px: { xs: 2, sm: 3 } }}>
 			<Typography variant="h4" component="h1" gutterBottom align="center">
 				{"Gestion des équipes organisateur"}
 			</Typography>
+			
+			<Button variant="contained" color="primary" onClick={() => navigate("/create-equipe-organiser")}>Créer une équipe</Button>
 		</Container>
 	)
 }
